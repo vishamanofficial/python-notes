2cc23ec6fcc8210086737821abe95d997ebffa74
Aman Vishwakarma <vishamanofficial@gmail.com>
2024-03-30 09:39:29 +0530

    notes

--- 07.py
+++ 07.py
@@ -1,2 +1,8 @@
 # File I/O in Python
 # Python can be used to perform operations on a file. (read & write data)
+
+f = open("data.txt", 'r')
+data  = f.read()
+print(data)
+print(type(data))
+f.close()
